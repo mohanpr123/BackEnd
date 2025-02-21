6955f255a4cb711b7ccc409be7b5b2034f46db6c
Mohan <mmohan8868@gmail.com>
2025-02-21 15:23:25 +0530

    Initial Commit

---
+++ src/main/java/com/example/Authentication/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.Authentication.repository;
+
+import com.example.Authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    boolean existsByEmail(String email);
+
+//    @Query("SELECT *FROM users WHERE user.password = :password")
+//    Optional<User> findByPassword(@Param("password") String password);
+
+    Optional<User> findByEmail(String email);
+
+}
